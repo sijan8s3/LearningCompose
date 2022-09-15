9423692c0fe97ab02a78c86d1a5dfa4c0f0f8c5d
Sijan <sijan@greatbear.tech>
2022-09-15 09:53:09 +0545

    basic sizing

--- app/src/main/java/com/sijanneupane/learningcompose/MainActivity.kt
+++ app/src/main/java/com/sijanneupane/learningcompose/MainActivity.kt
@@ -3,12 +3,16 @@ package com.sijanneupane.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.sijanneupane.learningcompose.ui.theme.LearningComposeTheme
 
@@ -16,7 +20,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-         Greeting(name = "Sijan")
+            Row {
+                Column(
+                    modifier = Modifier
+                        .background(Color.Gray)
+                        .fillMaxWidth(0.5f)
+                        .fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "World")
+                    Text(text = "World")
+                    Text(text = "World")
+                }
+
+                Row(
+                    Modifier
+                        .background(color = Color.Blue)
+                        .fillMaxHeight(0.5f)
+                        .fillMaxWidth(1f),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    Text(text = "Hello")
+                    Text(text = "World")
+                    Column() {
+                        Text(text = "Jetpack Compose",
+                            Modifier.fillMaxSize()
+                        )
+                    }
+                }
+
+            }
+
         }
     }
 }
